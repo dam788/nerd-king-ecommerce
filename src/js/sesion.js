3b0cfae86cb6db201ead7a01a15ef895cdf0f851
Hugo D. Morales <60529414+dam788@users.noreply.github.com>
2020-09-20 19:50:32 -0300

    se trabajo en el js y css del formulario de sesion

--- src/js/sesion.js
+++ src/js/sesion.js
@@ -1,27 +1,51 @@
 // seleccion del DOM
 const email     = document.getElementById('email'),
       pass      = document.getElementById('pass'),
-      submitBtn = document.getElementById('goForm');
+      submitBtn = document.getElementById('goForm'),
+      message   = document.getElementById('message');
 
 const entroEnFoco = elem => elem.className = 'enfoco';
+
 const salioDeFoco = elem => elem.className = '';
 
 const sendForm = (e) => {
+
     e.preventDefault();
-    if( validMail() && validPass() ) return true, window.location.href = '/'; ;
-    if(!validMail()) return false, alert('revisar Email');
-    if(!validPass()) return false, alert('revisar Password');
+
+    if( validMail() && validPass() ) return true, window.location.href = '/';
+
+    if(!validMail()){
+
+        message.classList.add('error');
+        setTimeout(()=>{
+            message.classList.remove('error');
+        },2000);
+        return ;
+    } 
+    if(!validPass()){
+
+        message.classList.add('error');
+        setTimeout(()=>{
+            message.classList.remove('error');
+        },2000);
+        return ;
+    } 
 } 
 
 const validMail = () => {
+
     let{ value } = email;
+
     const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;   
+
     return regex.test(value);
 }
 
 const validPass = () => {
     let{ value } = pass;
+
     const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+
     return regex.test(value);  
 }
 
