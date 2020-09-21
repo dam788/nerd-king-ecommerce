a584824256d723e2d895ade07bfc988cae5e4b80
micho19 <damiangiannico@gmail.com>
2020-09-21 16:49:34 -0300

    pag productos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-
 import './css/general.css';
 import './styles.css';
 import './css/header.css'
+import './css/footer.css'
 import './css/carousel.css'
 import './js/all.min'
 import './assets/logo.svg'
